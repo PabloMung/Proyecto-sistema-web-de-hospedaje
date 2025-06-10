54ac4b4e93f6fc532eb3517ff213954fa0c8fe0d
PabloMung <alter19recon@gmail.com>
2025-06-09 21:22:17 -0600

    base de datos

---
+++ proyecto_bd.sql
@@ -0,0 +1,237 @@
+CREATE DATABASE  IF NOT EXISTS `proyecto` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `proyecto`;
+-- MySQL dump 10.13  Distrib 8.0.40, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: proyecto
+-- ------------------------------------------------------
+-- Server version	8.0.40
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `actividad`
+--
+
+DROP TABLE IF EXISTS `actividad`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `actividad` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(255) NOT NULL,
+  `precio_adulto` int NOT NULL,
+  `precio_nino` int NOT NULL,
+  `descripcion` text,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `actividad`
+--
+
+LOCK TABLES `actividad` WRITE;
+/*!40000 ALTER TABLE `actividad` DISABLE KEYS */;
+INSERT INTO `actividad` VALUES (1,'Senderismo',100,250,'Caminata guiada por la montaña','2024-12-08 04:11:49','2024-12-11 14:47:05'),(4,'Tirolesa',300,200,'diversion',NULL,'2024-12-11 14:47:53'),(6,'tirolesa',200,100,'Gran aventura',NULL,NULL);
+/*!40000 ALTER TABLE `actividad` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `cabana`
+--
+
+DROP TABLE IF EXISTS `cabana`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cabana` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(255) NOT NULL,
+  `capacidad` int NOT NULL,
+  `costo` int NOT NULL,
+  `disponible` tinyint(1) NOT NULL DEFAULT '1',
+  `descripcion` text,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cabana`
+--
+
+LOCK TABLES `cabana` WRITE;
+/*!40000 ALTER TABLE `cabana` DISABLE KEYS */;
+INSERT INTO `cabana` VALUES (1,'Cabaña Familiar',8,1200,1,'Cabaña para familias grandes','2024-12-08 04:11:49','2025-06-09 15:59:48'),(3,'cabaña 13',4,100,1,'',NULL,'2024-12-11 23:28:28'),(8,'cabaña17',8,1000,1,'cabaña de buen espacio',NULL,'2024-12-16 21:36:34'),(11,'cabaña 15',1,199,0,'Una de las mejores cabañas',NULL,'2024-12-16 22:10:48'),(12,'cabaña11',5,100,1,'Gran comodidad',NULL,NULL),(13,'cabaña1',8,1000,1,'Para toda la familia',NULL,NULL),(14,'cabaña2',3,2000,1,'Buena para parejas',NULL,NULL),(15,'cabaña18',8,2500,1,'Gran espacio para toda la familia',NULL,NULL);
+/*!40000 ALTER TABLE `cabana` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `permiso`
+--
+
+DROP TABLE IF EXISTS `permiso`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `permiso` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `ruta` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `nombre` (`nombre`)
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `permiso`
+--
+
+LOCK TABLES `permiso` WRITE;
+/*!40000 ALTER TABLE `permiso` DISABLE KEYS */;
+INSERT INTO `permiso` VALUES (1,'ver-rol','2024-12-10 14:50:07','2024-12-12 19:01:06','/roles/index.xhtml'),(2,'crear-rol','2024-12-10 14:50:07','2024-12-12 19:01:06','/roles/crea.xhtml'),(3,'editar-rol','2024-12-10 14:50:07','2024-12-12 19:01:06','/roles/editar.xhtml'),(5,'ver-usuario','2024-12-10 14:50:07','2024-12-12 19:01:06','/usuarios/index.xhtml'),(6,'crear-usuario','2024-12-10 14:50:07','2024-12-12 19:01:06','/usuarios/crea.xhtml'),(7,'editar-usuario','2024-12-10 14:50:07','2024-12-12 19:01:06','/usuarios/editar.xhtml'),(9,'ver-cabana','2024-12-10 14:50:07','2024-12-12 19:01:06','/cabanas/index.xhtml'),(10,'crear-cabana','2024-12-10 14:50:07','2024-12-12 19:01:06','/cabanas/crea.xhtml'),(11,'editar-cabana','2024-12-10 14:50:07','2024-12-12 19:01:06','/cabanas/edit.xhtml'),(13,'ver-reservacion','2024-12-10 14:50:07','2024-12-12 19:01:06','/reservaciones/index.xhtml'),(14,'crear-reservacion','2024-12-10 14:50:07','2024-12-12 19:01:06','/reservaciones/crea.xhtml'),(15,'editar-reservacion','2024-12-10 14:50:07','2024-12-12 19:01:06','/reservaciones/editar.xhtml'),(17,'ver-actividad','2024-12-10 14:50:07','2024-12-12 19:01:06','/Actividad/index.xhtml'),(18,'crear-actividad','2024-12-10 14:50:07','2024-12-12 19:01:06','/Actividad/crea.xhtml'),(19,'editar-actividad','2024-12-10 14:50:07','2024-12-12 19:01:06','/Actividad/edit.xhtml'),(21,'ver-reserva','2024-12-10 14:50:07','2024-12-12 19:01:06','/reservaciones/indexRe.xhtml');
+/*!40000 ALTER TABLE `permiso` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `reservacion`
+--
+
+DROP TABLE IF EXISTS `reservacion`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `reservacion` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre_reservador` varchar(255) NOT NULL,
+  `numero_de_personas` int NOT NULL,
+  `fecha_entrada` date NOT NULL,
+  `fecha_salida` date NOT NULL,
+  `telefono` varchar(20) NOT NULL,
+  `cabana_id` int NOT NULL,
+  `usuario_id` int NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `usuario_id` (`usuario_id`),
+  KEY `fk_reservacion_cabana` (`cabana_id`),
+  CONSTRAINT `fk_reservacion_cabana` FOREIGN KEY (`cabana_id`) REFERENCES `cabana` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `reservacion_ibfk_2` FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=35 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `reservacion`
+--
+
+LOCK TABLES `reservacion` WRITE;
+/*!40000 ALTER TABLE `reservacion` DISABLE KEYS */;
+INSERT INTO `reservacion` VALUES (22,'Mich',5,'2025-06-06','2025-06-09','1234567890',1,61,NULL,'2025-06-09 16:27:07'),(30,'Mauro',4,'2025-06-02','2025-06-04','21321332',3,61,NULL,NULL),(33,'Pablo Alejandro M',5,'2025-06-07','2025-06-07','9512908631',13,61,NULL,'2025-06-09 19:06:27'),(34,'Pablo Alejandro M',6,'2025-06-06','2025-06-07','9512908631',15,61,NULL,NULL);
+/*!40000 ALTER TABLE `reservacion` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `rol`
+--
+
+DROP TABLE IF EXISTS `rol`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `rol` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `rol`
+--
+
+LOCK TABLES `rol` WRITE;
+/*!40000 ALTER TABLE `rol` DISABLE KEYS */;
+INSERT INTO `rol` VALUES (1,'Administrador','2024-12-08 04:11:49','2024-12-10 13:55:56'),(2,'Cliente','2024-12-08 04:11:49','2024-12-08 04:11:49');
+/*!40000 ALTER TABLE `rol` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `rol_permiso`
+--
+
+DROP TABLE IF EXISTS `rol_permiso`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `rol_permiso` (
+  `rol_id` int NOT NULL,
+  `permiso_id` int NOT NULL,
+  PRIMARY KEY (`rol_id`,`permiso_id`),
+  KEY `permiso_id` (`permiso_id`),
+  CONSTRAINT `rol_permiso_ibfk_1` FOREIGN KEY (`rol_id`) REFERENCES `rol` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `rol_permiso_ibfk_2` FOREIGN KEY (`permiso_id`) REFERENCES `permiso` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `rol_permiso`
+--
+
+LOCK TABLES `rol_permiso` WRITE;
+/*!40000 ALTER TABLE `rol_permiso` DISABLE KEYS */;
+INSERT INTO `rol_permiso` VALUES (1,1),(1,2),(1,3),(1,5),(1,6),(1,7),(1,9),(2,9),(1,10),(1,11),(1,13),(2,13),(1,14),(1,15),(1,17),(2,17),(1,18),(1,19),(1,21),(2,21);
+/*!40000 ALTER TABLE `rol_permiso` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuario`
+--
+
+DROP TABLE IF EXISTS `usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuario` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `contrasena` varchar(255) NOT NULL,
+  `rolId` int DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`),
+  KEY `rol` (`rolId`),
+  CONSTRAINT `rol` FOREIGN KEY (`rolId`) REFERENCES `rol` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=63 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuario`
+--
+
+LOCK TABLES `usuario` WRITE;
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+INSERT INTO `usuario` VALUES (61,'Pablo','admin@gmail.com','@Munguia19',1,NULL,'2025-06-09 05:57:01'),(62,'Mich','mich2016user@gmail.com','@Mich2016',1,NULL,'2025-06-09 05:57:54');
+/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-06-09 21:20:58
